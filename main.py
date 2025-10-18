3b928609212ca396cd0d97d7563ecb9406fe51c6
Manpreet Singh <singh.manpreet1308@gmail.com>
2025-10-18 11:27:11 +0100

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.Wine_Quality_Prediction import logger
 from src.Wine_Quality_Prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.Wine_Quality_Prediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,3 +15,13 @@ except Exception as e:
         raise e
 
 
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/Wine_Quality_Prediction/entity/config_entity.py
+++ src/Wine_Quality_Prediction/entity/config_entity.py
@@ -9,3 +9,12 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
+
+
+
